2dd74f12b8daf5e380b02116a9c0d845d8e7a820
Nikki Murello <nmurello15@gmail.com>
2022-09-14 18:22:27 -0400

    forgot to comment main.h :)

--- src/c/main.h
+++ src/c/main.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <pebble.h>
 
+// window and layer declarations for the entire program
 Window *main_window;
 Layer *flag_layer, *dots_layer, *analog_time_layer, *digital_time_layer;
 
+// defining all settings variables in a struct for the entire program
 typedef struct ClaySettings {
     //customizable
     GColor main_color;
@@ -19,6 +21,8 @@ typedef struct ClaySettings {
     int min_hand_size;
 } ClaySettings;
 
+// updating everything
 void update_stuff();
 
+// initalizing a ClaySettings named "settings"
 ClaySettings settings;
